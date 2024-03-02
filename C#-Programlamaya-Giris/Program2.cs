c93d89e83c7821d78da6dec757894b0a6e531dd8
Mehmethan AYTEKELİ <mehmethanaytekeli@gmail.com>
2024-03-03 02:46:55 +0300

    update

---
+++ C#-Programlamaya-Giris/Program2.cs
@@ -0,0 +1,4 @@
+﻿string isim;
+Console.Write("isminizi Girin:");
+isim=Console.ReadLine();
+Console.WriteLine("Merhaba "+ isim+" sisteme hoşgeldin...");
---
+++ C#-Programlamaya-Giris/Program3.cs
@@ -0,0 +1,5 @@
+﻿int dogumyili, yas;
+Console.Write("doğum yılınızı giriniz:");
+dogumyili = Convert.ToInt32(Console.ReadLine());
+yas = 2024 - dogumyili;
+Console.WriteLine(yas + " Yaşındasınız.");
